5164954cef53dd26929340c2251d9bf4c7b20df4
Bertrand Berthelot <bertrand.berthelot@broadpeak.tv>
2023-05-25 09:21:36 +0200

    Update ResourceTiming:
    - startTime property as required
    - add encodedBodySize property
    
    Signed-off-by: Bertrand Berthelot <bertrand.berthelot@broadpeak.tv>

--- lib/src/request/ResourceTiming.ts
+++ lib/src/request/ResourceTiming.ts
@@ -4,18 +4,18 @@
  * @group Request
  */
 export interface ResourceTiming {
-  // Returns the timestamp for the time a resource fetch started.
-  startTime?: number
+  // The timestamp for the time a resource fetch started.
+  startTime: number
 
-  // Returns a timestamp that is the difference between the responseEnd and the startTime properties.
-  duration?: number
-
-  // A number representing the size (in octets) of the fetched resource. The size includes the response header fields plus the response payload body.
-  transferSize?: number
+  // The size (in octets) of the payload body before removing any applied content encodings.
+  encodedBodySize?: number
   
-  // A timestamp immediately after the browser receives the first byte of the response from the server.
+  // The timestamp immediately after the browser receives the first byte of the response from the server.
   responseStart?: number
 
-  // A timestamp immediately after the browser receives the last byte of the resource or immediately before the transport connection is closed, whichever comes first.
+  // The timestamp immediately after the browser receives the last byte of the resource or immediately before the transport connection is closed, whichever comes first.
   responseEnd?: number
+
+  // The difference between the responseEnd and the startTime properties.
+  duration?: number
 }
