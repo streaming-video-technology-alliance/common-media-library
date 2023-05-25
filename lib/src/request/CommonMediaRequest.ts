6b56463f25d732e222c76d0dbe31429f7cabccb2
Bertrand Berthelot <bertrand.berthelot@broadpeak.tv>
2023-05-25 11:02:11 +0200

    Remove application based request execution functionnality
    Signed-off-by: Bertrand Berthelot <bertrand.berthelot@broadpeak.tv>

--- lib/src/request/CommonMediaRequest.ts
+++ lib/src/request/CommonMediaRequest.ts
@@ -52,46 +52,4 @@ export interface CommonMediaRequest {
    * Any custom data.
    */
   customData?: any
-
-  /**
-   * The progress event listener to be notified when the request receives more data.
-   * Note: a RequestPlugin that executes the request shall call back this listener (see RequestPlugin API)
-   */
-  onprogress?: (e: ProgressEvent) => void
-
-  /**
-   * The load event listener to be notified when the request transaction completes successfully.
-   * Note: a RequestPlugin that executes the request shall call back this listener (see RequestPlugin API)
-   */
-  onload?: (e?: ProgressEvent) => void
-
-  /**
-   * The loadend event listener to be notified when the request has completed (after load) successfully or unsuccessfully.
-   * Note: a RequestPlugin that executes the request shall call back this listener (see RequestPlugin API)
-   */
-  onloadend?: (e?: ProgressEvent) => void
-
-  /**
-   * The abort event listener to be notified when the request has been aborted.
-   * Note: a RequestPlugin that executes the request shall call back this listener (see RequestPlugin API)
-   */
-  onabort?: (e?: ProgressEvent) => void
-
-  /**
-   * The timeout event listener to be notified when progression is terminated due to preset time expiring.
-   * Note: a RequestPlugin that executes the request shall call back this listener (see RequestPlugin API)
-   */
-  ontimeout?: (e?: ProgressEvent) => void
-
-  /**
-   * The error event listener to be notified when the request encountered an error.
-   * Note: a RequestPlugin that executes the request shall call back this listener (see RequestPlugin API)
-   */
-  onerror?: (e: ErrorEvent) => void
-
-  /**
-   * The abort callback function to be called for aborting.
-   * Note: a RequestPlugin that executes the request shall provide this callback function (see RequestPlugin API)
-   */
-  abort?: () => void
 }
