d7d5dde1c16e3b90e6feb850b56684ee3b0dfe24
Bertrand Berthelot <bertrand.berthelot@broadpeak.tv>
2023-11-13 15:02:50 +0100

    Reorder export in alphabetical order
    Signed-off-by: Bertrand Berthelot <bertrand.berthelot@broadpeak.tv>

--- lib/src/request.ts
+++ lib/src/request.ts
@@ -3,8 +3,8 @@
  * 
  * @packageDocumentation
  */
-export type { ResourceTiming } from './request/ResourceTiming.js';
 export type { CommonMediaRequest } from './request/CommonMediaRequest.js';
 export type { CommonMediaResponse } from './request/CommonMediaResponse.js';
 export type { RequestInterceptor } from './request/RequestInterceptor.js';
 export type { ResponseInterceptor } from './request/ResponseInterceptor.js';
+export type { ResourceTiming } from './request/ResourceTiming.js';
