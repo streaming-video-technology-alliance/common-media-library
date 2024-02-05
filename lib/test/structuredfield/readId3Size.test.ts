300685c855f24f2885b68e86b657083166870d76
Fernando Cuadro <fernandoc@qualabs.com>
2024-02-05 14:35:49 -0300

    added 2 more unit tests
    Signed-off-by: Fernando Cuadro <fernandoc@qualabs.com>

--- lib/test/structuredfield/readId3Size.test.ts
+++ lib/test/structuredfield/readId3Size.test.ts
@@ -5,6 +5,6 @@ import { readId3Size } from '../../src/id3/util/readId3Size.js';
 describe('readId3Size', () => {
 	const mockID3SizeData = Uint8Array.from([73, 68, 51, 4, 0, 0, 0, 7, 1, 0]);
 	it('returns Id3 size', () => {
-		equal(readId3Size(mockID3SizeData, 6), 114688);
+		equal(readId3Size(mockID3SizeData, 6), 114816);
 	});
 });
