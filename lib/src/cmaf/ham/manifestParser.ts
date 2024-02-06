6a468f910d15fec060b596a06f7439ad0eecc4c2
Noelia Bentancor <71080743+NoeliaBentancor@users.noreply.github.com>
2024-02-06 11:19:49 -0300

    Change async functions in manifest parser

--- lib/src/cmaf/ham/manifestParser.ts
+++ lib/src/cmaf/ham/manifestParser.ts
@@ -1,12 +1,16 @@
-// @ts-ignore
 import { parseM3u8 } from '../utils/m3u8.js';
 import { Presentation } from './Presentation.js';
 import { SelectionSet } from './SelectionSet.js';
 import { SwitchingSet } from './SwitchingSet.js';
 import { uuid } from '../../utils.js';
 
-async function readHLS(manifestUrl: string) {
-    const response = await fetch(manifestUrl, {
+type FetchResponse = {
+    text: () => Promise<string>;
+};
+
+
+async function readHLS(manifestUrl: string): Promise<string> {
+    const response: FetchResponse = await fetch(manifestUrl, {
         headers: {
             'Content-Type': 'application/vnd.apple.mpegurl',
         }
@@ -14,38 +18,38 @@ async function readHLS(manifestUrl: string) {
     return response.text();
 }
 
-export async function m3u8toHam(url:string) {
-    const hls = await readHLS(url);
+export async function m3u8toHam(url: string): Promise<Presentation> {
+    const hls: string = await readHLS(url);
     const parsedM3u8 = parseM3u8(hls);
-    const segments = parsedM3u8.playlists;
-    let presentationSetDuration = 0;
-    let selectionSets:SelectionSet[] = [];
-    await segments.forEach(async (playlist:any) => {
-        let segmentUrl = url.split("/");
-        let segmentUrlWithoutLast = segmentUrl.slice(0, segmentUrl.length - 1).join("/").concat("/").concat(playlist.uri);
-        let parse = await readHLS(segmentUrlWithoutLast);
-        let parsedSegment = parseM3u8(parse);
-        let selectionSetDuration = 0;
-        let switchingSets :SwitchingSet[]= [];
-        await parsedSegment.segments.forEach(async(segment:any) => { 
+    const segments= parsedM3u8.playlists;
+
+    let presentationSetDuration: number = 0;
+    let selectionSets: SelectionSet[] = [];
+
+    await Promise.all(segments.map(async (playlist: any) => {
+        let segmentUrl: string[] = url.split("/");
+        let segmentUrlWithoutLast: string = segmentUrl.slice(0, segmentUrl.length - 1).join("/").concat("/").concat(playlist.uri);
+        let parse: string = await readHLS(segmentUrlWithoutLast);
+        let parsedSegment: any = parseM3u8(parse);
+        let selectionSetDuration: number = 0;
+        let switchingSets: SwitchingSet[] = [];
+
+        await Promise.all(parsedSegment.segments.map(async (segment: any) => {
             selectionSetDuration += segment.duration;
             presentationSetDuration += segment.duration;
-            let type = segment.map ? "video" : "audio";
-            let language = playlist.attributes.LANGUAGE;
+            let type: string = segment.map ? "video" : "audio";
+            let language: string = playlist.attributes.LANGUAGE;
             switchingSets.push(new SwitchingSet(uuid(), type, playlist.attributes.CODECS, segment.duration, language));
-        });
-        selectionSets.push(new SelectionSet(uuid(), selectionSetDuration, switchingSets));
-        selectionSetDuration = 0;
-        switchingSets = [];
-    });
+        }));
 
-    await new Promise<void>((resolve, reject) => {
+        selectionSets.push(new SelectionSet(uuid(), selectionSetDuration, switchingSets));
+    }));
+
+    await new Promise<void>((resolve) => {
         setTimeout(() => {
             resolve();
-        } , 1000);}
-    );
+        }, 1000);
+    });
 
-   
     return new Presentation(uuid(), presentationSetDuration, selectionSets);
 }
-
