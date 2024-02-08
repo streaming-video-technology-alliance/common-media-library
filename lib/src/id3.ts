6b3ed9f6b80fa68ce1f703313603186040216c9f
Casey Occhialini <1508707+littlespex@users.noreply.github.com>
2024-02-08 13:23:09 -0800

    doc: add missing group tag

--- lib/src/id3.ts
+++ lib/src/id3.ts
@@ -1,6 +1,9 @@
 /**
  * A collection of tools for working with ID3v2 tags.
  *
+ * @see {@link https://mutagen-specs.readthedocs.io/en/latest/id3/id3v2.4.0-structure.html | ID3v2.4.0 Structure}
+ * @see {@link https://mutagen-specs.readthedocs.io/en/latest/id3/id3v2.4.0-frames.html | ID3v2.4.0 Frames}
+ *
  * @packageDocumentation
  */
 export type { DecodedId3Frame } from './id3/DecodedId3Frame.js';
