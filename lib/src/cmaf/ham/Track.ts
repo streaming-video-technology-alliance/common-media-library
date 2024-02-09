58dfd3add2f92ab544e0eba95aaf61d83cbdb57c
Noelia Bentancor <71080743+NoeliaBentancor@users.noreply.github.com>
2024-02-09 14:05:36 -0300

    Add function is video track so as to parse framerate, width and height

--- lib/src/cmaf/ham/Track.ts
+++ lib/src/cmaf/ham/Track.ts
@@ -1,3 +1,5 @@
+import { VideoTrack } from "./VideoTrack";
+
 export abstract class Track{
     id: string;
     type: string;
@@ -13,5 +15,8 @@ export abstract class Track{
         this.duration = duration;
         this.language = language;
         this.bandwidth = bandwidth;
+    }
+    isVideoTrack(track: any): track is VideoTrack {
+        return track.width !== undefined && track.height !== undefined && track.frameRate !== undefined;
     }
 }
--- lib/src/cmaf/ham/manifestParser.ts
+++ lib/src/cmaf/ham/manifestParser.ts
@@ -1,22 +1,13 @@
 import { Presentation } from "./Presentation.js";
-import { VideoTrack } from "./VideoTrack.js";
-import { m3u8, PlayList, MediaGroups } from "./hlsManifest.js";
+import { m3u8, PlayList, MediaGroups ,Segment} from "./hlsManifest.js";
 const AUDIO_TYPE = "audio";
 const VIDEO_TYPE = "video";
-async function readHLS(manifestUrl: string): Promise<string> {
-    const response = await fetch(manifestUrl, {
-        headers: {
-            'Content-Type': 'application/vnd.apple.mpegurl',
-        }
-    });
-    return response.text();
-}
 
 
 export async function hamToM3u8(presentation: Presentation): Promise<m3u8> {
     let playlists: PlayList[] = [];
     let mediaGroups: MediaGroups= { AUDIO: {} };
-    let segments: any[] = [];
+    let segments: Segment[] = [];
     for (const selectionSet of presentation.selectionsSets) {
         for (const switchingSet of selectionSet.switchingsSet) {
             const {language, codec, type} = switchingSet;
@@ -32,20 +23,22 @@ export async function hamToM3u8(presentation: Presentation): Promise<m3u8> {
                 };
                 mediaGroups = { ...mediaGroups, ...mediaGroup };
             }else if(type == VIDEO_TYPE){
-                
-                let playlist: PlayList = {
-                    //TO DO : Check if we are saving uri
-                    uri: '',
-                    attributes: {
-                        CODECS: codec,
-                        BANDWIDTH: switchingSet.tracks[0].bandwidth,
-                        RESOLUTION: {width:0, height: 0},
-                        FRAME_RATE: 0
+                for (const track of switchingSet.tracks) {
+                    if (track.isVideoTrack(track)) {
+                        playlists.push({
+                            uri: '',
+                            attributes: {
+                                CODECS: codec,
+                                BANDWIDTH: track.bandwidth,
+                                FRAME_RATE: track.frameRate,
+                                RESOLUTION: {width: track.width, height: track.height},
+                            }
+    
+                        });
                     }
-                };
-                playlists.push(playlist);
+                }
             }
-            const segment: any = {
+            const segment: Segment = {
                 duration: switchingSet.duration
             };
             segments.push(segment);
