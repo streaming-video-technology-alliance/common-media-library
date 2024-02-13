a9bc16754cce63bae2792b546f2550428040dd07
hernan <hernanr@qualabs.com>
2024-02-13 14:56:21 -0300

    Added CEA-608/708 methods
    Signed-off-by: hernan <hernanr@qualabs.com>
    Signed-off-by: Casey Occhialini <1508707+littlespex@users.noreply.github.com>

---
+++ lib/src/cea.ts
@@ -0,0 +1,10 @@
+
+
+/**
+ * A collection of tools for working with ID3v2 tags.
+ *
+ * @packageDocumentation
+ */
+export { Row } from './cea/608/cea-608-parser.js';
+export { CaptionScreen } from './cea/608/cea-608-parser.js';
+export type { default as Cea608Parser } from './cea/608/cea-608-parser.js';
