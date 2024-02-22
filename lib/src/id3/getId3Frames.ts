4a3d742c152dff692297cd7e9f00d7e9ca4fa536
Felipe Young <121059628+fyoungqualabs@users.noreply.github.com>
2024-02-22 14:45:53 -0300

    test: add more ID3 tests
    see: #74 
    
    ---------
    
    Signed-off-by: Felipe Young <felipey@qualabs.com>
    Co-authored-by: Fernando Cuadro <fernandoc@qualabs.com>

--- lib/src/id3/getId3Frames.ts
+++ lib/src/id3/getId3Frames.ts
@@ -6,6 +6,9 @@ import { isId3Footer } from './util/isId3Footer.js';
 import { isId3Header } from './util/isId3Header.js';
 import { readId3Size } from './util/readId3Size.js';
 
+const HEADER_FOOTER_SIZE = 10;
+const FRAME_SIZE = 10;
+
 /**
  * Returns an array of ID3 frames found in all the ID3 tags in the id3Data
  *
@@ -24,10 +27,10 @@ export function getId3Frames(id3Data: Uint8Array): Id3Frame[] {
 	while (isId3Header(id3Data, offset)) {
 		const size = readId3Size(id3Data, offset + 6);
 		// skip past ID3 header
-		offset += 10;
+		offset += HEADER_FOOTER_SIZE;
 		const end = offset + size;
 		// loop through frames in the ID3 tag
-		while (offset + 8 < end) {
+		while (offset + FRAME_SIZE < end) {
 			const frameData: RawId3Frame = getId3FrameData(id3Data.subarray(offset));
 			const frame: Id3Frame | undefined = decodeId3Frame(frameData);
 			if (frame) {
@@ -39,7 +42,7 @@ export function getId3Frames(id3Data: Uint8Array): Id3Frame[] {
 		}
 
 		if (isId3Footer(id3Data, offset)) {
-			offset += 10;
+			offset += HEADER_FOOTER_SIZE;
 		}
 	}
 
