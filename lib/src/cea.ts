2f64eff87b1cf55da3c989e53bea5040941a885e
hernan <hernanr@qualabs.com>
2024-02-23 12:45:19 -0300

    Added captionScreen, cea608Channel and cea608Parser tests
    Signed-off-by: hernan <hernanr@qualabs.com>

--- lib/src/cea.ts
+++ lib/src/cea.ts
@@ -10,4 +10,5 @@ export { CaptionScreen } from './cea/608/cea-608-parser.js';
 export { CaptionsLogger } from './cea/608/cea-608-parser.js';
 export { PenState } from './cea/608/cea-608-parser.js';
 export { StyledUnicodeChar } from './cea/608/cea-608-parser.js';
-export type { default as Cea608Parser } from './cea/608/cea-608-parser.js';
+export { Cea608Channel } from './cea/608/cea-608-parser.js';
+export { default as Cea608Parser } from './cea/608/cea-608-parser.js';
