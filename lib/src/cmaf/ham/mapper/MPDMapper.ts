378455854261118484276cea852c9e38ae7f2dc5
Noelia Bentancor <71080743+NoeliaBentancor@users.noreply.github.com>
2024-02-26 11:15:19 -0300

    Add first approach of toHam method in mpd mapper

--- lib/src/cmaf/ham/mapper/MPDMapper.ts
+++ lib/src/cmaf/ham/mapper/MPDMapper.ts
@@ -1,11 +1,33 @@
+import { parseString } from 'xml2js';
+import { DashManifest } from '../../utils/dash/DashManifest.js';
 import { Manifest } from '../../utils/types/index.js';
 import { Presentation } from '../types/model/index.js';
 import { IMapper } from './IMapper.js';
+import { mapMpdToHam } from '../hamMapper.js';
 
 export class MPDMapper implements IMapper {
+	xmlToJson(raw: string, replace: (manifest: DashManifest) => void): void {
+		return parseString(raw, (err: Error | null, result: DashManifest) => {
+			if (err) {
+				throw new Error(err.message);
+			}
+			replace(result);
+		});
+	}
 	toHam(manifest: Manifest): Presentation[] {
-		console.log(manifest);
-		throw new Error('Not implemented');
+		let dashManifest: DashManifest | undefined;
+		this.xmlToJson(
+			manifest.main,
+			(result: DashManifest) => (dashManifest = result),
+		);
+
+		if (!dashManifest) {
+			return [];
+		}
+
+		const presentation = mapMpdToHam(dashManifest);
+
+		return [presentation];
 	}
 
 	toManifest(presentation: Presentation[]): Manifest {
