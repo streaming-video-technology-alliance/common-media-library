20d72c8068d17718ef5e945474ac57aefe22b5a7
Matías Rodriguez <matiasnrb97@gmail.com>
2024-02-26 16:40:48 -0300

    update Segment import in formatter.ts file

--- lib/src/cmaf/utils/hls/formatter.ts
+++ lib/src/cmaf/utils/hls/formatter.ts
@@ -1,4 +1,4 @@
-import { Segment } from '../../ham/model/Segment.js';
+import { Segment } from '../../ham/types/model/Segment.js';
 
 function formatSegmentUrl(url: string, segmentUrl: string) {
 	return url.split('/').slice(0, -1).join('/') + '/' + segmentUrl;
@@ -9,7 +9,7 @@ function formatSegments(segments: any[]) {
 	(segments.map(async (segment: any) => {
 		const { duration, uri } = segment;
 		const { length, offset } = segment.byterange ? segment.byterange : { length: 0, offset: 0 };
-		formattedSegments.push(new Segment(duration, uri, `${length}@${offset}`));
+		formattedSegments.push({ duration, url: uri, byteRange: `${length}@${offset}` });
 	}));
 
 	return formattedSegments;
