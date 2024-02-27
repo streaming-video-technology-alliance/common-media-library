fedefcb851229cf8623e11fe96502467e10cd900
Matias Rodriguez <matiasnrb97@gmail.com>
2024-02-27 16:33:51 -0300

    fix import

--- lib/src/cmaf/ham/mapper/HLSMapper.ts
+++ lib/src/cmaf/ham/mapper/HLSMapper.ts
@@ -1,5 +1,5 @@
 import { hamToM3U8 } from '../../utils/hls/hamToHLS.js';
-import { m3u8ToHam } from '../../utils/hls/HLSToHam.js';
+import { m3u8ToHam } from '../../utils/hls/hlsToHam.js';
 import { Manifest } from '../../utils/types/index.js';
 import { Presentation } from '../types/model/index.js';
 import { IMapper } from './IMapper.js';
