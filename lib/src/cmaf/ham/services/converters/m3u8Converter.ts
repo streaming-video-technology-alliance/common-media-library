ff8b3a621bf3807f7ae355646abfd4fcce39171d
Noelia Bentancor <71080743+NoeliaBentancor@users.noreply.github.com>
2024-02-28 16:24:52 -0300

    Add documentation for m3u8 converter

--- lib/src/cmaf/ham/services/converters/m3u8Converter.ts
+++ lib/src/cmaf/ham/services/converters/m3u8Converter.ts
@@ -2,18 +2,31 @@ import { HLSMapper } from '../../mapper/HLSMapper';
 import { MapperContext } from '../../mapper/MapperContext';
 import { Presentation } from '../../types/model';
 
-function m3u8ToHam(manifest: string, anciallaryManifests: string[]) {
+/**
+ * Convert hls manifest into a ham object.
+ *
+ * @param manifest - Main manifest.
+ * @param ancillaryManifests - Ancillary Manifests . Must be in order, first audio, subtitle and video.
+ */
+
+function m3u8ToHam(manifest: string, ancillaryManifests: string[]) {
 	const mapperContext = MapperContext.getInstance();
 	mapperContext.setStrategy(new HLSMapper());
 	return mapperContext.getHamFormat({
 		manifest,
-		ancillaryManifests: anciallaryManifests.map((ancillaryManifest) => ({
+		ancillaryManifests: ancillaryManifests.map((ancillaryManifest) => ({
 			manifest: ancillaryManifest,
 			type: 'm3u8',
 		})),
 		type: 'm3u8',
 	});
 }
+
+/**
+ * Convert ham object into a hls manifest.
+ *
+ * @param presentation - Ham object. List of presentations.
+ */
 
 function hamToM3U8(presentation: Presentation[]) {
 	const mapperContext = MapperContext.getInstance();
