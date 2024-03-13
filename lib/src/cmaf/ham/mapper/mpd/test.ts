2e16a08ec4dd0fab6fbad0b68c30472b32fdb74f
Noelia Bentancor <71080743+NoeliaBentancor@users.noreply.github.com>
2024-03-13 16:53:02 -0300

    Delete unnecesary test

--- lib/src/cmaf/ham/mapper/mpd/test.ts
+++
@@ -1,7 +0,0 @@
-import { mpdToHam } from "../../services/converters/mpdConverter";
-import fs from "fs";
-//convert mpd to ham
-const mpd = fs.readFileSync("test.mpd", "utf8");
-const ham = mpdToHam(mpd);
-console.log(ham);
-
