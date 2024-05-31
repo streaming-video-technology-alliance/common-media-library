e259eb1eb05732c3f6f83af49015d763c7db078d
Agajan Jumakuliyev <agajan.tm@gmail.com>
2024-05-30 22:36:54 -0700

    Create extractCea608DataFromSample.ts
    Signed-off-by: Agajan Jumakuliyev <agajan.tm@gmail.com>

---
+++ lib/src/cta/608/extractCea608DataFromSample.ts
@@ -0,0 +1,33 @@
+import {
+	getSeiData,
+	isSeiNalUnitType,
+	parseCea608DataFromSei,
+} from './utils/seiHelpers';
+
+/**
+ * Extracts CEA-608 data from a given sample.
+ *
+ * @param raw - The DataView with media data
+ * @param startPos - The start position within the DataView
+ * @param sampleSize - The size of the sample in bytes
+ * @returns fieldData array containing field 1 and field 2 data arrays
+ *
+ * @beta
+ */
+export function extractCea608DataFromSample(raw: DataView, startPos: number, sampleSize: number): number[][] {
+	let nalSize = 0;
+	let nalType = 0;
+	const fieldData: number[][] = [[], []];
+
+	for (let cursor = startPos; cursor < startPos + sampleSize - 5; cursor += nalSize + 4) {
+		nalSize = raw.getUint32(cursor, true);
+		nalType = raw.getUint8(cursor + 4) & 0x1F;
+
+		// Only process Supplemental Enhancement Information (SEI) NAL units
+		if (isSeiNalUnitType(nalType)) {
+			const seiData = getSeiData(raw, cursor + 5, cursor + nalSize + 4);
+			parseCea608DataFromSei(seiData, fieldData);
+		}
+	}
+	return fieldData;
+}
