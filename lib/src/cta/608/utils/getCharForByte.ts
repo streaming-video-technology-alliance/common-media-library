485e89fb865b60977ea93b8bc4d44b684e256a92
Agajan J <agajan.tm@gmail.com>
2024-06-05 13:31:49 -0700

    Issue/88-outstanding-608-prs (#92)
    * Create extractCea608DataFromSample.ts
    * Create seiHelpers.ts
    * Update extractCea608DataFromSample.ts
    * Update seiHelpers.ts
    * Update getCharForByte.ts
    * ported changes from video-dev/hls.js#6427
    * Create SccParser.ts
    * Update Cta608Parser.ts
    
    ---------
    
    Signed-off-by: Agajan Jumakuliyev <agajan.tm@gmail.com>
    Signed-off-by: Casey Occhialini <1508707+littlespex@users.noreply.github.com>

--- lib/src/cta/608/utils/getCharForByte.ts
+++ lib/src/cta/608/utils/getCharForByte.ts
@@ -1,11 +1,5 @@
 import { specialCea608CharsCodes } from './specialCea608CharsCodes.js';
 
-
 export const getCharForByte = function (byte: number): string {
-	let charCode = byte;
-	if (Object.prototype.hasOwnProperty.call(specialCea608CharsCodes, byte)) {
-		charCode = specialCea608CharsCodes[byte];
-	}
-
-	return String.fromCharCode(charCode);
+	return String.fromCharCode(specialCea608CharsCodes[byte] || byte);
 };
