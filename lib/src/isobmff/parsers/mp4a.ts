eaec76c417f86a140d56cabb9e5e0516572ecafa
Casey Occhialini <1508707+littlespex@users.noreply.github.com>
2024-11-13 23:30:37 -0800

    no message

--- lib/src/isobmff/parsers/mp4a.ts
+++ lib/src/isobmff/parsers/mp4a.ts
@@ -14,15 +14,17 @@ export type AudioSampleEntry = SampleEntry & {
 
 // ISO/IEC 14496-12:2012 - 8.5.2.2 mp4a box (use AudioSampleEntry definition and naming)
 export function mp4a(view: IsoView): AudioSampleEntry {
+	const { readArray, readUint, readTemplate, readData } = view;
+
 	return {
-		reserved1: view.readArray(UINT, 1, 6),
-		dataReferenceIndex: view.readUint(2),
-		reserved2: view.readArray(UINT, 4, 2),
-		channelcount: view.readUint(2),
-		samplesize: view.readUint(2),
-		preDefined: view.readUint(2),
-		reserved3: view.readUint(2),
-		samplerate: view.readTemplate(4),
-		esds: view.readData(-1),
+		reserved1: readArray(UINT, 1, 6),
+		dataReferenceIndex: readUint(2),
+		reserved2: readArray(UINT, 4, 2),
+		channelcount: readUint(2),
+		samplesize: readUint(2),
+		preDefined: readUint(2),
+		reserved3: readUint(2),
+		samplerate: readTemplate(4),
+		esds: readData(-1),
 	};
 };
