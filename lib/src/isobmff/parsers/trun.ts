58efc911d39046a19b24baf8847fbda406019a3a
Casey Occhialini <1508707+littlespex@users.noreply.github.com>
2025-01-21 11:19:57 -0800

    feat: add mdhd, tfra, and trun boxes
    Signed-off-by: Casey Occhialini <1508707+littlespex@users.noreply.github.com>

---
+++ lib/src/isobmff/parsers/trun.ts
@@ -0,0 +1,70 @@
+import type { FullBox } from '../FullBox.js';
+import type { IsoView } from '../IsoView.js';
+
+export type TrackRunSample = {
+	sampleDuration?: number;
+	sampleSize?: number;
+	sampleFlags?: number;
+	sampleCompositionTimeOffset?: number;
+}
+
+/**
+ * ISO/IEC 14496-12:2012 - 8.8.8 Track Run Box
+ *
+ * Note: the 'trun' box has a direct relation to the 'tfhd' box for defaults.
+ * These defaults are not set explicitly here, but are left to resolve for the user.
+ *
+ * @group ISOBMFF
+ *
+ * @beta
+ */
+export type TrackRunBox = FullBox & {
+	sampleCount: number;
+	dataOffset?: number;
+	firstSampleFlags?: number;
+	samples: TrackRunSample[];
+};
+
+// ISO/IEC 14496-12:2012 - 8.8.8 Track Run Box
+export function trun(view: IsoView): TrackRunBox {
+	const { version, flags } = view.readFullBox();
+	const sampleCount = view.readUint(4);
+	let dataOffset: number | undefined;
+	let firstSampleFlags: number | undefined;
+
+	if (flags & 0x1) {
+		dataOffset = view.readInt(4);
+	}
+
+	if (flags & 0x4) {
+		firstSampleFlags = view.readUint(4);
+	}
+
+	const samples = view.readEntries<TrackRunSample>(sampleCount, () => {
+		const sample: TrackRunSample = {};
+
+		if (flags & 0x100) {
+			sample.sampleDuration = view.readUint(4);
+		}
+		if (flags & 0x200) {
+			sample.sampleSize = view.readUint(4);
+		}
+		if (flags & 0x400) {
+			sample.sampleFlags = view.readUint(4);
+		}
+		if (flags & 0x800) {
+			sample.sampleCompositionTimeOffset = (version === 1) ? view.readInt(4) : view.readUint(4);
+		}
+
+		return sample;
+	});
+
+	return {
+		version,
+		flags,
+		sampleCount,
+		dataOffset,
+		firstSampleFlags,
+		samples,
+	};
+};
