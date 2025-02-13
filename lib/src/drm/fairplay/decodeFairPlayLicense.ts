3ee8241177764835f098d73432328774242d4cd4
Agajan Jumakuliyev <agajan.tm@gmail.com>
2025-02-13 13:36:40 -0800

    Create decodeFairPlayLicense.ts
    Signed-off-by: Agajan Jumakuliyev <agajan.tm@gmail.com>

---
+++ lib/src/drm/fairplay/decodeFairPlayLicense.ts
@@ -0,0 +1,17 @@
+import { base64decode } from '../../utils';
+
+/**
+ * Decodes a FairPlay DRM license response.
+ *
+ * @param response - The license response, either a base64 string or ArrayBuffer
+ * @returns A Uint8Array containing the decoded license
+ *
+ * @group DRM
+ * @beta
+ */
+
+export function decodeFairPlayLicense(response: string | ArrayBuffer): Uint8Array {
+	return typeof response === 'string'
+		? base64decode(response)
+		: new Uint8Array(response);
+}
