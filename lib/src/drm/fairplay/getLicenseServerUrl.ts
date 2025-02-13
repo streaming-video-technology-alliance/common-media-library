1555e4e9e1e32cd9230fe17e6e9516d6c9659d83
Agajan Jumakuliyev <agajan.tm@gmail.com>
2025-02-13 13:36:59 -0800

    Create getLicenseServerUrl.ts
    Signed-off-by: Agajan Jumakuliyev <agajan.tm@gmail.com>

---
+++ lib/src/drm/fairplay/getLicenseServerUrl.ts
@@ -0,0 +1,28 @@
+import { isEmpty } from '../utils/isEmpty';
+
+/**
+ * Retrieves the FairPlay license server URL from InitData or DRM configuration.
+ *
+ * @param initData - The initialization data (PSSH box)
+ * @param drmConfig - The DRM configuration containing the license URL
+ * @returns The license server URL as a string
+ *
+ * @group DRM
+ * @beta
+ */
+
+export function getLicenseServerUrl(initData: Uint16Array, drmConfig: { licenseUrl?: string; }): string {
+	if (!isEmpty(drmConfig.licenseUrl) && drmConfig.licenseUrl?.startsWith('http')) {
+		return drmConfig.licenseUrl;
+	}
+
+	let initDataString = '';
+	for (let i = 0; i < initData.length; i++) {
+		if (initData[i] !== 0) { // Ignore null characters
+			initDataString += String.fromCharCode(initData[i]);
+		}
+	}
+
+	const match = initDataString.match(/skd:\/\/([^"\s]+)/);
+	return match ? `https://${match[1]}` : '';
+}
