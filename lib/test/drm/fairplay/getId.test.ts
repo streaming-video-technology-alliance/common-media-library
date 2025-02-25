15bf485fec29979f2c9cc291470c6b5b545ff276
Agajan Jumakuliyev <agajan.tm@gmail.com>
2025-02-25 12:37:33 -0800

    Update getId.test.ts
    Added one more test case
    
    Signed-off-by: Agajan Jumakuliyev <agajan.tm@gmail.com>

--- lib/test/drm/fairplay/getId.test.ts
+++ lib/test/drm/fairplay/getId.test.ts
@@ -3,6 +3,14 @@ import { strictEqual } from 'node:assert';
 import { describe, it } from 'node:test';
 
 describe('getId', () => {
+	it('extracts contentId from skd:// format', () => {
+		const laUrl = 'https://common-media-library.com';
+		const initDataString = 'skd://common-media-library.com/asset1234';
+		const initData = new TextEncoder().encode(initDataString);
+
+		strictEqual(getId(laUrl, initData), 'common-media-library.com/asset1234');
+	});
+	
 	it('extracts query param when provided', () => {
 		const laUrl = 'https://common-media-library.com?videoId=svta-cml';
 		const initData = new Uint16Array();
