89a92e5498f34fa1708a6f4430fa687df83c27c9
Agajan Jumakuliyev <agajan.tm@gmail.com>
2025-02-26 10:44:50 -0800

    Update getLicenseServerUrl.test.ts
    drmConfig param was removed, so needed to update its test accordingly.
    
    Signed-off-by: Agajan Jumakuliyev <agajan.tm@gmail.com>

--- lib/test/drm/fairplay/getLicenseServerUrl.test.ts
+++ lib/test/drm/fairplay/getLicenseServerUrl.test.ts
@@ -3,17 +3,13 @@ import { strictEqual } from 'node:assert';
 import { describe, it } from 'node:test';
 
 describe('getLicenseServerUrl', () => {
-	it('returns licenseUrl from drmConfig when it is available', () => {
-		const initData = new Uint16Array();
-		const drmConfig = { licenseUrl: 'https://common-media-library.com' };
-
-		strictEqual(getLicenseServerUrl(initData, drmConfig), 'https://common-media-library.com');
+	it('extracts URL from initData when skd:// is present', () => {
+		const initData = new Uint16Array([...new TextEncoder().encode('skd://common-media-library.com')]);
+		strictEqual(getLicenseServerUrl(initData), 'https://common-media-library.com');
 	});
 
-	it('extracts URL from initData when licenseUrl is not available', () => {
-		const initData = new Uint16Array([...new TextEncoder().encode('skd://common-media-library.com')]);
-		const drmConfig = {};
-
-		strictEqual(getLicenseServerUrl(initData, drmConfig), 'https://common-media-library.com');
+	it('returns an empty string when skd:// is missing', () => {
+		const initData = new Uint16Array([...new TextEncoder().encode('invalid-initData')]);
+		strictEqual(getLicenseServerUrl(initData), '');
 	});
 });
