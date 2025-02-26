1ad2801853a78f37169ae2dd3bc65cc3486d7856
Agajan Jumakuliyev <agajan.tm@gmail.com>
2025-02-26 10:43:18 -0800

    Update getLicenseServerUrl.ts
    Getting rid of unnecessary drmConfig param.
    
    Signed-off-by: Agajan Jumakuliyev <agajan.tm@gmail.com>

--- lib/src/drm/fairplay/getLicenseServerUrl.ts
+++ lib/src/drm/fairplay/getLicenseServerUrl.ts
@@ -1,21 +1,14 @@
-import { isEmpty } from '../utils/isEmpty.js';
-
 /**
- * Retrieves the FairPlay license server URL from InitData or DRM configuration.
+ * Retrieves the FairPlay license server URL from InitData.
  *
  * @param initData - The initialization data (PSSH box)
- * @param drmConfig - The DRM configuration containing the license URL
  * @returns The license server URL as a string
  *
  * @group DRM
  * @beta
  */
 
-export function getLicenseServerUrl(initData: Uint16Array, drmConfig: { licenseUrl?: string; }): string {
-	if (!isEmpty(drmConfig.licenseUrl) && drmConfig.licenseUrl?.startsWith('http')) {
-		return drmConfig.licenseUrl;
-	}
-
+export function getLicenseServerUrl(initData: Uint16Array): string {
 	let initDataString = '';
 	for (let i = 0; i < initData.length; i++) {
 		if (initData[i] !== 0) { // Ignore null characters
