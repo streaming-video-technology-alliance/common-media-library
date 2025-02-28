dceb81b4b2caad8a3e277425d76677cf966c27c1
Agajan Jumakuliyev <agajan.tm@gmail.com>
2025-02-28 14:35:25 -0800

    Create drm.ts
    Signed-off-by: Agajan Jumakuliyev <agajan.tm@gmail.com>

---
+++ lib/src/drm.ts
@@ -0,0 +1,12 @@
+/**
+ * A collection of tools for working with Apple's Fairplay DRM.
+ *
+ * @packageDocumentation
+ *
+ * @beta
+ */
+export * from './drm/fairplay/getId.js';
+export * from './drm/fairplay/getLicenseServerUrl.js';
+export * from './drm/fairplay/extractContentId.js';
+export * from './drm/fairplay/decodeFairPlayLicense.js';
+export * from './drm/fairplay/concatInitDataIdAndCertificate.js';
