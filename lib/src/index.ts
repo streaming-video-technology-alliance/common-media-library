bd1f9a2556bab87175d9b86db115992bd66847a4
Agajan Jumakuliyev <agajan.tm@gmail.com>
2025-02-28 14:33:01 -0800

    Update index.ts
    Signed-off-by: Agajan Jumakuliyev <agajan.tm@gmail.com>

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -9,6 +9,7 @@ export * from './cmcd.js';
 export * from './cmsd.js';
 export type * from './cta.js';
 export * from './dash.js';
+export * from './drm.js';
 export * from './id3.js';
 export * from './iso8601.js';
 export * from './isobmff.js';
