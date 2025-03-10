48cccae398561788143cf830dd411ddcb146a2a1
Will Harris <will.harris.dv@gmail.com>
2025-03-10 17:47:37 -0400

    Add drm constants
    Signed-off-by: Will Harris <will.harris.dv@gmail.com>

---
+++ lib/src/drm/const/CLEAR_KEY_SYSTEM.ts
@@ -0,0 +1,8 @@
+/**
+ * Clear Key System.
+ *
+ * @group DRM
+ *
+ * @beta
+ */
+export const CLEAR_KEY_SYSTEM = 'org.w3.clearkey';
---
+++ lib/src/drm/const/WIDEVINE_KEY_SYSTEM.ts
@@ -0,0 +1,8 @@
+/**
+ * Widevine Key System.
+ *
+ * @group DRM
+ *
+ * @beta
+ */
+export const WIDEVINE_KEY_SYSTEM = 'com.widevine.alpha';
