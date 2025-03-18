aedd024c7ef764997c828fbac32a3d3e120a1fcd
Agajan Jumakuliyev <agajan.tm@gmail.com>
2025-03-18 11:23:03 -0700

    Update drm.ts
    Signed-off-by: Agajan Jumakuliyev <agajan.tm@gmail.com>

--- lib/src/drm.ts
+++ lib/src/drm.ts
@@ -1,12 +1,56 @@
 /**
- * A collection of tools for working with Apple's Fairplay DRM.
+ * A collection of tools for working with DRM.
  *
  * @packageDocumentation
  *
  * @beta
  */
+// fairplay utilities
 export * from './drm/fairplay/getId.js';
 export * from './drm/fairplay/getLicenseServerUrl.js';
 export * from './drm/fairplay/extractContentId.js';
 export * from './drm/fairplay/decodeFairPlayLicense.js';
 export * from './drm/fairplay/concatInitDataIdAndCertificate.js';
+
+// drm models
+export type { KeySystemConfiguration } from './drm/models/KeySystemConfiguration.js';
+export type { KeyMessage } from './drm/models/KeyMessage.js';
+export type { KeySystemAccess } from './drm/models/KeySystemAccess.js';
+export type { LicenseRequest } from './drm/models/LicenseRequest.js';
+export type { MediaCapability } from './drm/models/MediaCapability.js';
+
+// drm constants
+export * from './drm/common/const/CBCS.js';
+export * from './drm/common/const/CENC.js';
+export * from './drm/common/const/CLEAR_KEY_SYSTEM.js';
+export * from './drm/common/const/CLEAR_KEY_UUID.js';
+export * from './drm/common/const/ENCRYPTION_SCHEME.js';
+export * from './drm/common/const/EXPIRED.js';
+export * from './drm/common/const/HW_SECURE_ALL.js';
+export * from './drm/common/const/HW_SECURE_CRYPTO.js';      
+export * from './drm/common/const/HW_SECURE_DECODE.js';
+export * from './drm/common/const/INDIVIDUALIZATION_REQUEST.js';
+export * from './drm/common/const/INITIALIZATION_DATA_TYPE.js';
+export * from './drm/common/const/INTERNAL_ERROR.js';
+export * from './drm/common/const/KEYIDS.js';
+export * from './drm/common/const/LICENSE_RELEASE.js';
+export * from './drm/common/const/LICENSE_RENEWAL.js';
+export * from './drm/common/const/LICENSE_REQUEST.js';
+export * from './drm/common/const/MEDIA_KEY_MESSAGE_TYPES.js';
+export * from './drm/common/const/MEDIA_KEY_STATUS.js';
+export * from './drm/common/const/OUTPUT_DOWNSCALED.js';
+export * from './drm/common/const/OUTPUT_RESTRICTED.js';
+export * from './drm/common/const/PLAYREADY_KEY_SYSTEM.js';
+export * from './drm/common/const/PLAYREADY_RECOMMENDATION_KEY_SYSTEM.js';
+export * from './drm/common/const/PLAYREADY_UUID.js';
+export * from './drm/common/const/RELEASED.js';
+export * from './drm/common/const/STATUS_PENDING.js';
+export * from './drm/common/const/SW_SECURE_CRYPTO.js';
+export * from './drm/common/const/SW_SECURE_DECODE.js';
+export * from './drm/common/const/USABLE.js';
+export * from './drm/common/const/W3C_CLEAR_KEY_UUID.js';
+export * from './drm/common/const/WEBM.js';
+export * from './drm/common/const/WEBM.js';
+export * from './drm/common/const/WIDEVINE_KEY_SYSTEM.js';
+export * from './drm/common/const/WIDEVINE_ROBUSTNESS.js';
+export * from './drm/common/const/WIDEVINE_UUID.js';
