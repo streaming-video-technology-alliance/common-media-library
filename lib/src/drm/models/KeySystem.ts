7717e500c5c4f675d4fcf6a9a14e6acba64721f4
Agajan Jumakuliyev <agajan.tm@gmail.com>
2025-03-20 14:43:30 -0700

    Update KeySystem.ts
    Needed to add systemString field
    
    Signed-off-by: Agajan Jumakuliyev <agajan.tm@gmail.com>

--- lib/src/drm/models/KeySystem.ts
+++ lib/src/drm/models/KeySystem.ts
@@ -14,5 +14,11 @@ export type KeySystem = {
    * Optional scheme ID URI for the key system
    */
   schemeIdURI?: string;
+
+  /**
+   * The system string for the key system,
+   * such as `com.widevine.alpha` 
+   */
+  systemString: string;
 };
   
