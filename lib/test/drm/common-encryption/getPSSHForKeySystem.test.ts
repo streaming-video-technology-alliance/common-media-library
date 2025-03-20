3a7c8d68fb95b29cc936abbf8738a7a7465abb28
Agajan Jumakuliyev <agajan.tm@gmail.com>
2025-03-20 14:42:58 -0700

    Update getPSSHForKeySystem.test.ts
    Signed-off-by: Agajan Jumakuliyev <agajan.tm@gmail.com>

--- lib/test/drm/common-encryption/getPSSHForKeySystem.test.ts
+++ lib/test/drm/common-encryption/getPSSHForKeySystem.test.ts
@@ -2,12 +2,14 @@ import { getPSSHForKeySystem } from '@svta/common-media-library/drm/common-encry
 import { strictEqual } from 'node:assert';
 import { describe, it } from 'node:test';
 import { samplePsshBox } from '../common/samplePsshBox';
+import { WIDEVINE_KEY_SYSTEM } from '../../../src/drm/common/const/WIDEVINE_KEY_SYSTEM';
 
 describe('getPSSHForKeySystem', () => {
 	// sample keySystem UUID. It matches the systemId in the
 	// samplePsshBox in the common folder.
-	const keySystem: { uuid: string } = {
+	const keySystem: { uuid: string, systemString: string } = {
 		uuid: '1077efec-c0b2-4d02-ace3-3c1e52e2fb4b', 
+		systemString: WIDEVINE_KEY_SYSTEM, 
 	};
 
 	it('should return the correct PSSH box for the given key system', () => {
@@ -21,7 +23,8 @@ describe('getPSSHForKeySystem', () => {
 
 	it('should return null if key system does not match', () => {
 		const initData = new Uint8Array(samplePsshBox).buffer;
-		const result = getPSSHForKeySystem({ uuid: '00000000-0000-0000-0000-000000000000' }, initData);
+		const result = getPSSHForKeySystem({ uuid: '00000000-0000-0000-0000-000000000000', 
+			systemString: WIDEVINE_KEY_SYSTEM }, initData);
 
 		strictEqual(result, null);
 	});
