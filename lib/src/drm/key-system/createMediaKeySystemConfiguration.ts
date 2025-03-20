7a907eab9221313e9ff0777526bcd49e1dceb227
Agajan Jumakuliyev <agajan.tm@gmail.com>
2025-03-20 14:45:05 -0700

    Create createMediaKeySystemConfiguration.ts
    Signed-off-by: Agajan Jumakuliyev <agajan.tm@gmail.com>

---
+++ lib/src/drm/key-system/createMediaKeySystemConfiguration.ts
@@ -0,0 +1,34 @@
+import type { MediaCapability } from '../models/MediaCapability.js';
+import type { KeySystemConfiguration } from '../models/KeySystemConfiguration.js';
+
+/**
+ * Creates a valid KeySystemConfiguration from supported audio and video capabilities.
+ *
+ * @param supportedAudio - supported audio capabilities.
+ * @param supportedVideo - supported video capabilities.
+ * @returns KeySystemConfiguration object.
+ *
+ * @group DRM
+ * @beta
+ */
+export function createMediaKeySystemConfiguration(
+	supportedAudio: MediaCapability[] | null,
+	supportedVideo: MediaCapability[] | null,
+): KeySystemConfiguration {
+	const config: KeySystemConfiguration = {};
+
+	if (supportedAudio && supportedAudio.length > 0) {
+		config.audioCapabilities = supportedAudio;
+	}
+
+	if (supportedVideo && supportedVideo.length > 0) {
+		config.videoCapabilities = supportedVideo;
+	}
+
+	// default values 
+	config.distinctiveIdentifier = 'optional';
+	config.persistentState = 'optional';
+	config.sessionTypes = ['temporary'];
+
+	return config;
+}
