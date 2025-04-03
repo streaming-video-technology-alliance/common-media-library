876d1a66c994efffd3c4aebd1178ff1077febb1b
Agajan Jumakuliyev <agajan.tm@gmail.com>
2025-04-03 13:48:33 -0700

    Update parseInitDataFromContentProtection.ts
    Signed-off-by: Agajan Jumakuliyev <agajan.tm@gmail.com>

--- lib/src/drm/cemc/parseInitDataFromContentProtection.ts
+++ lib/src/drm/cemc/parseInitDataFromContentProtection.ts
@@ -18,8 +18,8 @@ export function parseInitDataFromContentProtection(
 	cpData: ContentProtection,
 	BASE64: { decodeArray: (input: string) => Uint8Array },
 ): ArrayBuffer | null {
-	if (cpData?.pssh && cpData.pssh.__text) {
-		const cleanedText = cpData.pssh.__text.replace(/\r?\n|\r/g, '').replace(/\s+/g, '');
+	if (cpData?.pssh && cpData.pssh) {
+		const cleanedText = cpData.pssh.replace(/\r?\n|\r/g, '').replace(/\s+/g, '');
 		return BASE64.decodeArray(cleanedText).buffer as ArrayBuffer;;
 	}
 
