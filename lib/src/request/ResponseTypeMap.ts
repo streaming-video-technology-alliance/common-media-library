6c38f9ade41b0324d290f3e10296f06a68f25e88
Casey Occhialini <1508707+littlespex@users.noreply.github.com>
2025-04-03 14:25:55 -0700

    no message
    Signed-off-by: Casey Occhialini <1508707+littlespex@users.noreply.github.com>

--- lib/src/request/ResponseTypeMap.ts
+++ lib/src/request/ResponseTypeMap.ts
@@ -9,8 +9,8 @@ import type { XmlNode } from '../xml/XmlNode.js';
  */
 export type ResponseTypeMap<T extends string | undefined> =
 	T extends 'json' ? any :
-	T extends 'text' ? string :
-	T extends 'blob' ? Blob :
-	T extends 'arraybuffer' ? ArrayBuffer :
-	T extends 'document' ? XmlNode :
-	unknown;
+		T extends 'text' ? string :
+			T extends 'blob' ? Blob :
+				T extends 'arraybuffer' ? ArrayBuffer :
+					T extends 'document' ? XmlNode :
+						unknown;
