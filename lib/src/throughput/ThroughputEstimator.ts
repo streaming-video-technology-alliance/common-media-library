d77d771f579f280bbf21327f06e04b9b03b469df
Casey Occhialini <1508707+littlespex@users.noreply.github.com>
2025-04-15 15:59:15 -0700

    chore: add api comments
    Signed-off-by: Casey Occhialini <1508707+littlespex@users.noreply.github.com>

--- lib/src/throughput/ThroughputEstimator.ts
+++ lib/src/throughput/ThroughputEstimator.ts
@@ -10,10 +10,15 @@ import type { ResourceTiming } from '../request/ResourceTiming'
 export type ThroughputEstimator = {
 	/**
 	 * Adds a sample to the throughput estimator
+	 *
+	 * @param sample - The timing sample to add
 	 */
 	sample(sample: ResourceTiming): void
+
 	/**
 	 * Returns the current estimate of the throughput (in bytes per second)
+	 *
+	 * @returns The current estimate of the throughput
 	 */
 	getEstimate(): number
 }
