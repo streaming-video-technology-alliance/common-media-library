393da26a0b3e01a611b2d0e59a0248a73407bbea
Casey Occhialini <1508707+littlespex@users.noreply.github.com>
2025-05-07 11:18:33 -0700

    no message
    Signed-off-by: Casey Occhialini <1508707+littlespex@users.noreply.github.com>

--- lib/src/id3/ID3_SCHEME_ID_URI.ts
+++ lib/src/id3/ID3_SCHEME_ID_URI.ts
@@ -1,1 +1,10 @@
+/**
+ * The scheme ID URI for ID3 tags.
+ *
+ * @group ID3
+ *
+ * @beta
+ *
+ * @see {@link https://aomediacodec.github.io/id3-emsg/ | Carriage of ID3 Timed Metadata in the Common Media Application Format (CMAF)}
+ */
 export const ID3_SCHEME_ID_URI = 'https://aomedia.org/emsg/ID3';
