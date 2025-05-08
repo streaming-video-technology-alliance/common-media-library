9641ee0e1012834d0ab69ba58fe7bb64ee926f1b
willdharris <66787651+willdharris@users.noreply.github.com>
2025-05-08 11:37:19 -0400

    Add test
    Signed-off-by: Will Harris <will.harris.dv@gmail.com>
    Signed-off-by: willdharris <will.harris.dv@gmail.com>
    Signed-off-by: Casey Occhialini <1508707+littlespex@users.noreply.github.com>

---
+++ lib/test/drm/playready/getLicenseRequestFromMessage.test.ts
@@ -0,0 +1,22 @@
+import { base64decode } from '@svta/common-media-library/utils/base64decode';
+import { getLicenseRequestFromMessage } from '@svta/common-media-library/drm/playready/getLicenseRequestFromMessage';
+import { ENCODING_UTF16 } from '@svta/common-media-library/drm/common/ENCODING_UTF16';
+import { PLAYREADY_KEY_MESSAGE } from './data/PLAYREADY_KEY_MESSAGE.ts';
+import { deepStrictEqual, strictEqual } from 'node:assert';
+import { describe, it } from 'node:test';
+
+const createUTF16Buffer = (input: string): ArrayBuffer => Buffer.from(input, 'utf16le').buffer;
+
+describe('getLicenseRequestFromMessage', () => {
+	it('returns the original message if byte length is odd and encoding is UTF-16', () => {
+		const message = createUTF16Buffer('jAm0n');
+		const result = getLicenseRequestFromMessage(message, ENCODING_UTF16);
+		strictEqual(result, message);
+	});
+
+	it('finds the Challenge in the PlayReadyKeyMessage and returns the value as ArrayBuffer', () => {
+		const message = createUTF16Buffer(PLAYREADY_KEY_MESSAGE);
+		const result = getLicenseRequestFromMessage(message);
+		deepStrictEqual(result, base64decode('1beR1c0').buffer as ArrayBuffer);
+	});
+});
