07af3374efaad6aecb9aeb71db205b9c48b6403e
Juan Manuel Gonzalez <juanmanuel@qualabs.com>
2025-06-03 17:03:31 +0000

    Added descriptions from CMCD V2 Docs to CMCD V2 Event Mode keys

--- lib/src/cmcd/CmcdV2Event.ts
+++ lib/src/cmcd/CmcdV2Event.ts
@@ -8,6 +8,34 @@ export const CMCD_V2_EVENT_MODE_KEYS = [
  * CMCD v2 - Event and Event-Response keys.
  */
 export type CmcdV2Event = {
-	/** Reporting event (event mode; e.g. "e", "t", "ps") */
+	/** Reporting event (event mode; e.g. "e", "t", "ps")
+	 * 
+	 * This key MUST only be used in Event mode.
+	 * 
+	 * ps - play state change. This token MUST be accompanied by a 'sta' key carrying the new state.
+	 * 
+	 * e - the player has experienced an error. This token MUST be accompanied by a 'ec' key defining the player error code.
+	 * 
+	 * t - time interval. The interval at which these reports are made is application-defined. A default interval of 30 seconds SHOULD 
+	 * be used if no explicit application interval is provided. Short form content may wish to use a shorter interval. 
+	 * An application-defined interval of zero should be interpreted as turning off interval event reporting. 
+	 * This event MUST be supported by all players that support Event mode.
+	 * 
+	 * c - content ID has changed. This token MUST be accompanied by a 'cid' key defining the new content ID.
+	 * 
+	 * b - the player has entered backgrounded mode if this event is accompanied by the ‘bg’ key and exited backgrounded mode if not.
+	 * 
+	 * m - mute. The user activated the mute control or set the volume to zero.
+	 * 
+	 * um - unmute. The user deactivated the mute control or raised the volume above zero if it was previously set to zero.
+	 * 
+	 * pe - playerExpand. The user activated a control to extend the player to a larger size. The definition of this event is intended to be 
+	 * compliant with the VAST [VAST] Player Operation Metrics.
+	 * 
+	 * c - playerCollapse: the user activated a control to reduce the player to a smaller size. The definition of this event is intended to be
+	 * compliant with the VAST [VAST] Player Operation Metrics.
+	 * 
+	 * Token - one of [ps,e,t,c,b,m,u m, abs, abe, as, ae]
+	 */
 	e?: CmcdReportingEvent;
 };
