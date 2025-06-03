679bc4478fa91d01b3a2f6832d78de50f14a6cd9
Juan Manuel Gonzalez <juanmanuel@qualabs.com>
2025-06-03 18:05:48 +0000

    Added missing reporting events

--- lib/src/cmcd/CmcdReportingEvent.ts
+++ lib/src/cmcd/CmcdReportingEvent.ts
@@ -1,11 +1,18 @@
 /**
  * CMCD v2 event types for the 'e' key (event mode).
- * e: error, t: time interval, ps: play state change
  */
+
+
 export const CMCD_REPORTING_EVENTS = {
+	PLAY_STATE: 'ps',
 	ERROR: 'e',
 	TIME_INTERVAL: 't',
-	PLAY_STATE: 'ps',
+	CONTENT_ID: 'c',
+	BACKGROUNDED_MODE: 'b',
+	MUTE: 'm',
+	UNMUTE: 'um',
+	PLAYER_EXPAND: 'pe',
+	PLAYER_COLLAPSE: 'pc',
 } as const;
 
 export type CmcdReportingEvent = typeof CMCD_REPORTING_EVENTS[keyof typeof CMCD_REPORTING_EVENTS];
