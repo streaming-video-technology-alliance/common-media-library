61b1e8bcaa503a955b382abcd499cd9ff50ebaa5
Alexey Berezin <2991847+Beraliv@users.noreply.github.com>
2025-06-08 18:48:42 +0100

    feat: HarmonicMeanEstimator  (#199)
    see: #116
    
    ---------
    
    Signed-off-by: Alexey Berezin <2991847+Beraliv@users.noreply.github.com>

--- lib/src/throughput.ts
+++ lib/src/throughput.ts
@@ -1,3 +1,4 @@
 export type { ThroughputEstimator } from './throughput/ThroughputEstimator.js';
 export { Ewma } from './throughput/Ewma.js';
 export { EwmaEstimator } from './throughput/EwmaEstimator.js';
+export { HarmonicMeanEstimator } from './throughput/HarmonicMeanEstimator.js';
