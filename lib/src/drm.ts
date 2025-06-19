4799beb3ab089dffe70f4730436744f3ea8fe52a
Casey Occhialini <1508707+littlespex@users.noreply.github.com>
2025-06-18 22:01:06 -0700

    chore: remove errant export
    Signed-off-by: Casey Occhialini <1508707+littlespex@users.noreply.github.com>

--- lib/src/drm.ts
+++ lib/src/drm.ts
@@ -33,7 +33,6 @@ export * from './drm/playready/getRequestHeadersFromMessage.js';
 
 // drm types
 export type { ContentProtection } from './drm/common/ContentProtection.js';
-export type { KeyMessage } from './drm/common/KeyMessage.js';
 export type { LicenseRequest } from './drm/common/LicenseRequest.js';
 export type { MediaKeySystemAccessRequest } from './drm/common/MediaKeySystemAccessRequest.js';
 
