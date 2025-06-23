8ac897c495fee66c48689e4e155af2f833e494c9
Juan Manuel Gonzalez <juanmanuel@qualabs.com>
2025-06-23 12:36:56 -0300

    Updated CmcdPlayerState class

--- lib/src/cmcd/CmcdPlayerState.ts
+++ lib/src/cmcd/CmcdPlayerState.ts
@@ -1,25 +1,48 @@
+import type { ValueOf } from '../utils/ValueOf.js';
+
 /**
  * CMCD v2 player states for the 'sta' key.
- * s: starting, 
- * p: playing, 
- * k: seeking, 
- * r: rebuffering, 
- * a: paused, 
- * w: waiting,
- * e: ended, 
- * f: fatal error
+ * 
+ * @group CMCD
+ * @enum
+ * @beta
 */
-
-export const CMCD_PLAYER_STATES = {
+export const CmcdPlayerState = {
+	/**
+	 * Starting: Initial startup of the player.
+	 */
 	STARTING: 's',
+	/**
+	 * Playing: The player is actively rendering content.
+	 */
 	PLAYING: 'p',
+	/**
+	 * Seeking: The player is seeking to a new position.
+	 */
 	SEEKING: 'k',
+	/**
+	 * Rebuffering: The player is buffering data during playback.
+	 */
 	REBUFFERING: 'r',
+	/**
+	 * Paused: The player is paused.
+	 */
 	PAUSED: 'a',
+	/**
+	 * Waiting: The player is waiting for a user action or another event.
+	 */
 	WAITING: 'w',
+	/**
+	 * Ended: The media has finished playing.
+	 */
 	ENDED: 'e',
+	/**
+	 * Fatal Error: The player has encountered a fatal error.
+	 */
 	FATAL_ERROR: 'f',
 } as const;
 
-
-export type CmcdPlayerState = typeof CMCD_PLAYER_STATES[keyof typeof CMCD_PLAYER_STATES];
+/**
+ * @beta
+ */
+export type CmcdPlayerState = ValueOf<typeof CmcdPlayerState>;
