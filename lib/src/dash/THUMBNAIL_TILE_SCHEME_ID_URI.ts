ee9ecbe34a940afe79be1f963b7c6cd50598fa85
Casey Occhialini <1508707+littlespex@users.noreply.github.com>
2025-07-18 11:35:04 -0700

    feat: add const for dash thumbnail file scheme id uri
    Signed-off-by: Casey Occhialini <1508707+littlespex@users.noreply.github.com>

---
+++ lib/src/dash/THUMBNAIL_TILE_SCHEME_ID_URI.ts
@@ -0,0 +1,8 @@
+/**
+ * The scheme ID URI for thumbnail tiles in DASH.
+ *
+ * @group DASH
+ *
+ * @beta
+ */
+export const THUMBNAIL_TILE_SCHEME_ID_URI = 'http://dashif.org/guidelines/thumbnail_tile';
