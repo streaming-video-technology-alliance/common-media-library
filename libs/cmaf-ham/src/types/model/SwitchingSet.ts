41a5311b6315ac2d0dcbf5514b6b75ea334c9e2e
Christian Pillsbury <cpillsbury@mux.com>
2026-01-06 06:48:34 -0800

    fix: migrate new SwitchingSet. Remove parent from def.

--- libs/cmaf-ham/src/types/model/SwitchingSet.ts
+++ libs/cmaf-ham/src/types/model/SwitchingSet.ts
@@ -1,11 +1,17 @@
+import type { Base } from './Base.ts'
+import type { Protection } from './content-protection/Protection.ts'
 import type { Ham } from './Ham.ts'
-import type { Track } from './Track.ts'
+import type { Track } from '../Track.ts'
 
 /**
- * CMAF-HAM SwitchingSet type
+ * CMAF-HAM Switching Set type
  *
  * @alpha
  */
-export type SwitchingSet = Ham & {
+export type SwitchingSet = Ham & Base & {
 	tracks: Track[];
+	protection?: Protection;
+
+	// TODO: Is this a DASH specific term? - Yes: Revisit for shareable concepts/categorization, e.g. CHARACTERISTICS + values on #EXT-X-MEDIA
+	role?: string;
 };
