7c321a1def3ee5c17e6ee18591ca87afc5d0f757
Christian Pillsbury <cpillsbury@mux.com>
2026-01-06 06:49:26 -0800

    fix: Remove parent from Segment def.

--- libs/cmaf-ham/src/types/model/Segment.ts
+++ libs/cmaf-ham/src/types/model/Segment.ts
@@ -1,7 +1,6 @@
 import type { AddressableObject } from './AddressableObject.ts'
 import type { Duration } from './Duration.ts'
 import type { Ham } from './Ham.ts'
-import type { Track } from './Track.ts'
 
 /**
  * CMAF-HAM Segment type
@@ -9,6 +8,5 @@ import type { Track } from './Track.ts'
  * @alpha
  */
 export type Segment = Ham & AddressableObject & Duration & {
-	parent: Track;
 	startTime: number;
 };
