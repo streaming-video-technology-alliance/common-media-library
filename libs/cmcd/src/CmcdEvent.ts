fe5f7030ae8c22b67e096fe1792508f836a07322
Casey Occhialini <1508707+littlespex@users.noreply.github.com>
2026-01-15 09:42:10 -0800

    fix: remove response data type
    Signed-off-by: Casey Occhialini <1508707+littlespex@users.noreply.github.com>

--- libs/cmcd/src/CmcdEvent.ts
+++ libs/cmcd/src/CmcdEvent.ts
@@ -15,6 +15,26 @@ import type { CmcdRequest } from './CmcdRequest.ts'
  */
 export type CmcdEvent = Omit<CmcdRequest, CmcdEventExcludedKeys> & {
 	/**
+	 * CMSD Dynamic Header (response mode)
+	 *
+	 * Holds a Base64 [base64] encoded copy of the CMSD data received on the CMSD-Dynamic response header.
+	 * This key MUST only be used in RESPONSE mode.
+	 *
+	 * String
+	*/
+	cmsdd?: string;
+
+	/**
+	 * CMSD Static Header (response mode)
+	 *
+	 * Holds a Base64 [base64] encoded copy of the CMSD data received on the CMSD-Static response header.
+	 * This key MUST only be used in RESPONSE mode.
+	 *
+	 * String
+	*/
+	cmsds?: string;
+
+	/**
 	 * Event (event mode; e.g. "e", "t", "ps")
 	 *
 	 * This key MUST only be used in Event mode.
@@ -45,4 +65,75 @@ export type CmcdEvent = Omit<CmcdRequest, CmcdEventExcludedKeys> & {
 	 * Token - one of [ps,e,t,c,b,m,u m, abs, abe, as, ae]
 	 */
 	e?: CmcdEventType;
+
+	/**
+	 * Response code for object request (response mode)
+	 *
+	 * The response code received when requesting a media object. In a redirect scenario, this would be the final response code received.
+	 * A value of 0 SHOULD be used to indicate that a response was not received.
+	 *
+	 * Integer
+	*/
+	rc?: number;
+
+	/**
+	 * SMRT Header
+
+	 * Holds a Base64 [base64] encoded copy of the streaming media response tracing data received on the SMRT-Data header.
+	 * This key MUST only be used in RESPONSE mode.
+	 */
+	smrt?: number;
+
+	/**
+	 * Timestamp (ms since UNIX epoch, required for event mode)
+	 *
+	 * The timestamp at which the associated event occurred, expressed as milliseconds since the UNIX epoch.
+	 * When the event is a request for a media object the time SHOULD reference when the request was first initiated.
+	 * When used with Response Mode, the timestamp should indicate the time at which the object was first requested and not when it was received.
+	 *
+	 * Integer milliseconds
+	*/
+	ts?: number;
+
+	/**
+	 * Time to first byte (ms; response mode)
+	 *
+	 * The elapsed time between when the request was first initiated (captured in ts) and the time when the first byte of the response was received.
+	 * This value should only be reported if it is known. Absence of this key does not indicate that the response was not received.
+	 *
+	 * Integer milliseconds
+	*/
+	ttfb?: number;
+
+	/**
+	 * Time to first body byte (ms; response mode)
+	 *
+	 * The elapsed time between when the request was first initiated (captured in ts) and the time the first bytes of the response body are received.
+	 * This value should only be reported if it is known. Absence of this key does not indicate that the body was not received.
+	 *
+	 * Integer milliseconds
+	*/
+	ttfbb?: number;
+
+	/**
+	 * Time to last byte (ms; response mode)
+	 *
+	 * The elapsed time between when the request was first initiated (captured in ts) and the time the response body is fully received.
+	 * This value should only be reported if it is known.
+	 * Absence of this key does not indicate that the response was not fully received
+	 *
+	 * Integer milliseconds
+	*/
+	ttlb?: number;
+
+	/**
+	 * Requested URL (string; response mode)
+	 *
+	 * The URL used to request the media object.
+	 * This key MUST NOT be used with reporting mode #1.
+	 * If the request is redirected, this key MUST report the initial requested URL.
+	 *
+	 * String
+	*/
+	url?: string;
 };
