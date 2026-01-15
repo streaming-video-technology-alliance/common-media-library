38bed5456f9976caf5f21de14e8713cb19e4e3a9
Casey Occhialini <1508707+littlespex@users.noreply.github.com>
2026-01-14 17:07:18 -0800

    no message
    Signed-off-by: Casey Occhialini <1508707+littlespex@users.noreply.github.com>

--- libs/cmcd/test/data/QUERY_TARGET.ts
+++
@@ -1,8 +0,0 @@
-import { CmcdReportingMode, CmcdTransmissionMode, type CmcdReportTarget } from '@svta/cml-cmcd'
-
-export const QUERY_TARGET: CmcdReportTarget = {
-	url: 'https://hello.world',
-	version: 2,
-	reportingMode: CmcdReportingMode.EVENT,
-	transmissionMode: CmcdTransmissionMode.QUERY,
-}
