fed7fe2bc8718374b5938b34f150c6c800fdf8f8
Casey Occhialini <1508707+littlespex@users.noreply.github.com>
2026-02-11 20:21:09 -0800

    chore: add unit test for uint8array use case of parsePsshList
    Signed-off-by: Casey Occhialini <1508707+littlespex@users.noreply.github.com>

--- libs/drm/test/cenc/parsePsshList.test.ts
+++ libs/drm/test/cenc/parsePsshList.test.ts
@@ -1,5 +1,5 @@
 import { parsePsshList } from '@svta/cml-drm'
-import { deepStrictEqual, strictEqual } from 'node:assert'
+import { deepStrictEqual, ok, strictEqual } from 'node:assert'
 import { describe, it } from 'node:test'
 import { samplePsshBox } from '../common/samplePsshBox.ts'
 
@@ -17,5 +17,19 @@ describe('parsePsshList', () => {
 		const psshBox = result[expectedUUID]
 		strictEqual(psshBox.byteLength, samplePsshBox.length) // length should match
 		deepStrictEqual(new Uint8Array(psshBox), samplePsshBox)  // actual data should match
+	})
+
+	it('should parse PSSH boxes from a Uint8Array', () => {
+		const initData = new Uint8Array(samplePsshBox)
+		const result = parsePsshList(initData)
+		const expectedUUID = '1077efec-c0b2-4d02-ace3-3c1e52e2fb4b'
+
+		strictEqual(Object.keys(result).length, 1)
+		strictEqual(Object.keys(result)[0], expectedUUID)
+
+		const psshBox = result[expectedUUID]
+		ok(psshBox instanceof Uint8Array, 'result value should be a Uint8Array')
+		strictEqual(psshBox.byteLength, samplePsshBox.length)
+		deepStrictEqual(psshBox, samplePsshBox)
 	})
 })
