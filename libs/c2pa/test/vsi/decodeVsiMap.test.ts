9e2413b9fc63d81f6f2ff9fdf82f95d8c36ac33d
Nicolas Caballero <nicolascaballero.v@gmail.com>
2026-04-10 11:47:30 -0300

    fix(c2pa): type manifestId as string per C2PA tstr spec
    Signed-off-by: Nicolas Caballero <nicolascaballero.v@gmail.com>

--- libs/c2pa/test/vsi/decodeVsiMap.test.ts
+++ libs/c2pa/test/vsi/decodeVsiMap.test.ts
@@ -1,14 +1,30 @@
 import { decodeVsiMap } from '../../src/vsi/decodeVsiMap.ts'
-import { throws } from 'node:assert'
+import { deepStrictEqual, strictEqual, throws } from 'node:assert'
 import { describe, it } from 'node:test'
+import { encode } from 'cbor-x/encode'
 
 describe('decodeVsiMap', () => {
 	// #region example
+	it('decodes a valid VSI map with string manifestId', () => {
+		const hash = new Uint8Array([0xaa, 0xbb, 0xcc])
+		const vsiCbor = encode({
+			sequenceNumber: 7,
+			bmffHash: { hash, alg: 'sha256', exclusions: [] },
+			manifestId: 'urn:c2pa:12345',
+		})
+		const result = decodeVsiMap(new Uint8Array(vsiCbor))
+		strictEqual(result.sequenceNumber, 7)
+		strictEqual(result.manifestId, 'urn:c2pa:12345')
+		strictEqual(result.bmffHash.alg, 'SHA-256')
+		deepStrictEqual(result.bmffHash.hash, hash)
+		deepStrictEqual(result.bmffHash.exclusions, [])
+	})
+	// #endregion example
+
 	it('throws for non-object CBOR input', () => {
 		// CBOR integer 42 = 0x18 0x2a
 		throws(() => decodeVsiMap(new Uint8Array([0x18, 0x2a])), /VSI map/)
 	})
-	// #endregion example
 
 	it('throws when sequenceNumber is missing', () => {
 		// CBOR empty map {}
@@ -17,5 +33,14 @@ describe('decodeVsiMap', () => {
 
 	it('throws for invalid CBOR input', () => {
 		throws(() => decodeVsiMap(new Uint8Array([0xff, 0xff])))
+	})
+
+	it('throws when manifestId is not a string', () => {
+		const vsiCbor = encode({
+			sequenceNumber: 1,
+			bmffHash: { hash: new Uint8Array([0x01]), alg: 'sha256', exclusions: [] },
+			manifestId: new Uint8Array([0x01, 0x02]),
+		})
+		throws(() => decodeVsiMap(new Uint8Array(vsiCbor)), /manifestId/)
 	})
 })
